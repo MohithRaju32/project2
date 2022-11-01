7f254a13f5157fe72f0cb3a691a006b3c13631ce
Mohith Raju <mohithraju32@gmail.com>
2022-11-01 12:56:15 +0530

    My First Commit

---
+++ src/table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class table {
+    public static void main(String[] args) {
+        Scanner sd = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sd.nextInt();
+        int i;
+        for (i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d =%d\n", n, i, n * i);
+
+        }
+
+
+    }
+}
+
+
